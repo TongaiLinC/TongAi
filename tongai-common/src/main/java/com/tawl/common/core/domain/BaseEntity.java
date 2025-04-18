9246899de81780c4b22c393a6f28c7c78fa2c03c
TongAiLinC <tongyilinc@126.com>
2025-04-18 16:46:38 +0800

    Entity基类添加@Data注解

--- tongai-common/src/main/java/com/tawl/common/core/domain/BaseEntity.java
+++ tongai-common/src/main/java/com/tawl/common/core/domain/BaseEntity.java
@@ -1,8 +1,10 @@
 package com.tawl.common.core.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -14,12 +16,14 @@ import java.util.Map;
  * 
  * @author tongai
  */
+@Data
 public class BaseEntity implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
     /** 搜索值 */
     @JsonIgnore
+    @TableField(exist = false)
     private String searchValue;
 
     /** 创建者 */
@@ -42,63 +46,9 @@ public class BaseEntity implements Serializable
     /** 删除标志（0代表存在 2代表删除） */
     private String delFlag;
 
-    public String getSearchValue() {
-        return searchValue;
-    }
-
-    public void setSearchValue(String searchValue) {
-        this.searchValue = searchValue;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public String getDelFlag() {
-        return delFlag;
-    }
-
-    public void setDelFlag(String delFlag) {
-        this.delFlag = delFlag;
-    }
-
     /** 请求参数 */
+
+    @TableField(exist = false)
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Map<String, Object> params;
 
@@ -109,10 +59,5 @@ public class BaseEntity implements Serializable
             params = new HashMap<>();
         }
         return params;
-    }
-
-    public void setParams(Map<String, Object> params)
-    {
-        this.params = params;
     }
 }
