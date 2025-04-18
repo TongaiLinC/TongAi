73146ca9b804d66a2a0c390737251cf134715f5d
TongAiLinC <tongyilinc@126.com>
2025-04-18 16:52:04 +0800

    添加文件大小格式化工具类

--- tongai-ui/src/utils/tongai.js
+++ tongai-ui/src/utils/tongai.js
@@ -231,3 +231,12 @@ export function tansParams(params) {
 export function blobValidate(data) {
   return data.type !== 'application/json'
 }
+
+// 文件大小格式化
+export function formatFileSize(bytes) {
+  if (bytes === 0) return '0 Bytes'
+  const k = 1024
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
+}
